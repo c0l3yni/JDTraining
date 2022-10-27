08bd16dd98843816fd9664d6534cb224cc1f93c4
Cole Sulak <nsulak@teksystems.com>
2022-10-27 10:49:53 -0500

    Added results calculation between user and computer

--- Javascript/ROCK-PAPER-SCISSORS/app.js
+++ Javascript/ROCK-PAPER-SCISSORS/app.js
@@ -6,10 +6,11 @@ const possibleChoice = document.querySelectorAll("button");
 possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
 userChoice = e.target.id;
 userChoiceDisplay.innerHTML = userChoice;
-computerChoiceDisplay.innerHTML = computerChoice()
+computerChoiceDisplay.innerHTML = computerChoice();
+resultDisplay.innerHTML = result();
 }))
 
- function computerChoice() {
+function computerChoice() {
     let randomizer = Math.floor(Math.random() * 3 )
 
     if(randomizer === 0) {
@@ -19,7 +20,23 @@ computerChoiceDisplay.innerHTML = computerChoice()
     } else if(randomizer === 2) {
         option = "scissors"
     }
-    return computerChoice
+    return option
 }
 
-
+function result() {
+    if (userChoice == computerChoiceDisplay.innerHTML) {
+        return "tie"
+    } else if (userChoiceDisplay.innerHTML == "rock" && computerChoiceDisplay.innerHTML == "scissors") {
+        return "Computer LOSES"
+    } else if (userChoiceDisplay.innerHTML == "paper" && computerChoiceDisplay.innerHTML == "rock") {
+        return "Computer LOSES"
+    } else if (userChoiceDisplay.innerHTML == "scissors" && computerChoiceDisplay.innerHTML == "paper") {
+        return "Computer LOSES"
+    } else if (userChoiceDisplay.innerHTML == "rock" && computerChoiceDisplay.innerHTML == "paper") {
+        return "You LOSE"
+    } else if (userChoiceDisplay.innerHTML == "paper" && computerChoiceDisplay.innerHTML == "scissors") {
+        return "You LOSE"
+    } else if (userChoiceDisplay.innerHTML == "scissors" && computerChoiceDisplay.innerHTML == "rock") {
+        return "You LOSE"
+    }
+}
